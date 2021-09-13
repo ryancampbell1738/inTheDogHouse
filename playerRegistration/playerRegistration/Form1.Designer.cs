56c9a72c4009822695b5bac54f348251af3c8d75
ryancampbell1738 <90245584+ryancampbell1738@users.noreply.github.com>
2021-09-13 15:00:23 +0100

    Add files via upload

---
+++ playerRegistration/playerRegistration/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿
+namespace playerRegistration
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl = new System.Windows.Forms.TabControl();
+            this.tabDisplay = new System.Windows.Forms.TabPage();
+            this.tabAdd = new System.Windows.Forms.TabPage();
+            this.tabEdit = new System.Windows.Forms.TabPage();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.tabControl.SuspendLayout();
+            this.tabDisplay.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl
+            // 
+            this.tabControl.Controls.Add(this.tabDisplay);
+            this.tabControl.Controls.Add(this.tabAdd);
+            this.tabControl.Controls.Add(this.tabEdit);
+            this.tabControl.Location = new System.Drawing.Point(12, 94);
+            this.tabControl.Name = "tabControl";
+            this.tabControl.SelectedIndex = 0;
+            this.tabControl.Size = new System.Drawing.Size(776, 445);
+            this.tabControl.TabIndex = 0;
+            // 
+            // tabDisplay
+            // 
+            this.tabDisplay.Controls.Add(this.radioButton4);
+            this.tabDisplay.Controls.Add(this.radioButton3);
+            this.tabDisplay.Controls.Add(this.radioButton2);
+            this.tabDisplay.Controls.Add(this.radioButton1);
+            this.tabDisplay.Controls.Add(this.dataGridView1);
+            this.tabDisplay.Location = new System.Drawing.Point(4, 22);
+            this.tabDisplay.Name = "tabDisplay";
+            this.tabDisplay.Padding = new System.Windows.Forms.Padding(3);
+            this.tabDisplay.Size = new System.Drawing.Size(768, 419);
+            this.tabDisplay.TabIndex = 0;
+            this.tabDisplay.Text = "Display";
+            this.tabDisplay.UseVisualStyleBackColor = true;
+            this.tabDisplay.Click += new System.EventHandler(this.tabDisplay_Click);
+            // 
+            // tabAdd
+            // 
+            this.tabAdd.Location = new System.Drawing.Point(4, 22);
+            this.tabAdd.Name = "tabAdd";
+            this.tabAdd.Padding = new System.Windows.Forms.Padding(3);
+            this.tabAdd.Size = new System.Drawing.Size(768, 419);
+            this.tabAdd.TabIndex = 1;
+            this.tabAdd.Text = "Add";
+            this.tabAdd.UseVisualStyleBackColor = true;
+            // 
+            // tabEdit
+            // 
+            this.tabEdit.Location = new System.Drawing.Point(4, 22);
+            this.tabEdit.Name = "tabEdit";
+            this.tabEdit.Padding = new System.Windows.Forms.Padding(3);
+            this.tabEdit.Size = new System.Drawing.Size(768, 419);
+            this.tabEdit.TabIndex = 2;
+            this.tabEdit.Text = "Edit";
+            this.tabEdit.UseVisualStyleBackColor = true;
+            this.tabEdit.Click += new System.EventHandler(this.Edit_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(6, 6);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(521, 407);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(562, 44);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(44, 17);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Add";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(562, 87);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(43, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Edit";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(562, 120);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(56, 17);
+            this.radioButton3.TabIndex = 1;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Delete";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(562, 143);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(42, 17);
+            this.radioButton4.TabIndex = 1;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Exit";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 551);
+            this.Controls.Add(this.tabControl);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl.ResumeLayout(false);
+            this.tabDisplay.ResumeLayout(false);
+            this.tabDisplay.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl;
+        private System.Windows.Forms.TabPage tabDisplay;
+        private System.Windows.Forms.TabPage tabAdd;
+        private System.Windows.Forms.TabPage tabEdit;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+    }
+}
+
