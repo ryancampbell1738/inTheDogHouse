7f3f4732f681cf83485e50418e434462fe39dce5
ryanc2112 <30004700@live.nwrc.ac.uk>
2021-09-15 17:43:59 +0100

    Added MyException and MyValidation

---
+++ playerRegistration/playerRegistration/MyExcption.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace playerRegistration
+{
+    class MyExcption : Exception
+    {
+        private String message;
+
+        public MyExcption(String message)
+        {
+            this.message = message;
+        }
+
+        public String toString()
+        {
+            return String.Format("Error: {0}", message);
+        }
+    }
+}
---
+++ playerRegistration/playerRegistration/MyValidation.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace playerRegistration
+{
+    class MyValidation
+    {
+        public static bool validLength(String txt, int min, int max)
+        {
+            bool ok = true;
+
+            if (String.IsNullOrEmpty(txt))
+                ok = false;
+
+            else if (txt.Length < min || txt.Length > max)
+                ok = false;
+
+            return ok;
+        }
+
+        public static bool validNumber(string txt)
+        {
+            bool ok = true;
+            for(int x=0; x<txt.Length; x++)
+            {
+                if(!(char.IsNumber(txt[x])))
+                {
+                    ok = false;
+                }
+            }
+            return ok;
+        }
+
+        public static bool validLetter(String txt)      //Allows Alphabetic Characters
+        {
+            bool ok = true;
+
+            if (txt.Trim().Length == 0)
+            {
+                ok = false;
+            }
+            else
+            {
+                for(int x = 0; x < txt.Length; x++)
+                {
+                    if (!(char.IsLetter(txt[x])))
+                        ok = false;
+                }
+            }
+            return ok;
+        }
+
+        public static bool validLetterWhiteSpace(String txt)        // Allows Alphabetic Characters and Whitespace
+        {
+            bool ok = true;
+
+            if(txt.Trim().Length == 0)
+            {
+                ok = false;
+            }
+            else
+            {
+                for(int x = 0; x < txt.Length; x++)
+                {
+                    if (!(char.IsLetter(txt[x])) && !(char.IsWhiteSpace(txt[x])))
+                            ok = false;
+                }
+            }
+            return ok;
+        }
+    }
+}
